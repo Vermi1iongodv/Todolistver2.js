8ace6a0ff6735eb2b48689857df6066e57737812
Vermi1iongodv <92727945+Vermi1iongodv@users.noreply.github.com>
2022-10-28 13:32:33 +0300

    change js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
     todos = JSON.parse(localStorage.getItem('todos')) || [];
-    const nameInput = document.querySelector('name');
+    const nameInput = document.querySelector('#name');
     const newTodoForm = document.querySelector('#new-todo-form');
 
     const username = localStorage.getItem('username') || '';
